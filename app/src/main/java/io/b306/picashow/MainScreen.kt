57e2c9ee5778104d350508733e3086d210ad4a08
박태흠 <xomamoc@naver.com>
2023-10-30 09:54:51 +0900

    fix: Bottom NavBar 조건문 해결

--- app/src/main/java/io/b306/picashow/MainScreen.kt
+++ app/src/main/java/io/b306/picashow/MainScreen.kt
@@ -96,80 +96,44 @@ fun MainScreen(navController: NavHostController) {
                     composable("thirdPage") { ThirdPage() }
                     composable("addSchedulePage") { AddSchedulePage() }
                 }
-
-//            }
-//                val bottomNavItems = listOf(
-//                    BottomNavigationItem(
-//                        icon = {
-//                            Image(
-//                                painter = painterResource(id = R.drawable.calender),
-//                                contentDescription = null
-//                            )
-//                        },
-//                        selected = navController.currentDestination?.route == "firstPage",
-//                        onClick = { navController.navigate("firstPage") }
-//                    ),
-//                    BottomNavigationItem(
-//                        icon = {
-//                            Image(
-//                                painter = painterResource(id = R.drawable.today),
-//                                contentDescription = null
-//                            )
-//                        },
-//                        selected = navController.currentDestination?.route == "secondPage" || navController.previousBackStackEntry == null,
-//                        onClick = { navController.navigate("secondPage") }
-//                    ),
-//                    BottomNavigationItem(
-//                        icon = {
-//                            Image(
-//                                painter = painterResource(id = R.drawable.paintdiary),
-//                                contentDescription = null
-//                            )
-//                        },
-//                        selected = navController.currentDestination?.route == "thirdPage",
-//                        onClick = { navController.navigate("thirdPage") }
-//                    )
-//                )
-//            if (showAppBarAndNavBar) {
-//                BottomNavigation(
-//                    items = bottomNavItems
-//                )
             }
-            val bottomNavItems = listOf(
-                BottomNavigationItem(
-                    icon = {
-                        Image(
-                            painter = painterResource(id = R.drawable.calender),
-                            contentDescription = null
-                        )
-                    },
-                    selected = navController.currentDestination?.route == "firstPage",
-                    onClick = { navController.navigate("firstPage") }
-                ),
-                BottomNavigationItem(
-                    icon = {
-                        Image(
-                            painter = painterResource(id = R.drawable.today),
-                            contentDescription = null
-                        )
-                    },
-                    selected = navController.currentDestination?.route == "secondPage" || navController.previousBackStackEntry == null,
-                    onClick = { navController.navigate("secondPage") }
-                ),
-                BottomNavigationItem(
-                    icon = {
-                        Image(
-                            painter = painterResource(id = R.drawable.paintdiary),
-                            contentDescription = null
-                        )
-                    },
-                    selected = navController.currentDestination?.route == "thirdPage",
-                    onClick = { navController.navigate("thirdPage") }
+                val bottomNavItems = listOf(
+                    BottomNavigationItem(
+                        icon = {
+                            Image(
+                                painter = painterResource(id = R.drawable.calender),
+                                contentDescription = null
+                            )
+                        },
+                        selected = navController.currentDestination?.route == "firstPage",
+                        onClick = { navController.navigate("firstPage") }
+                    ),
+                    BottomNavigationItem(
+                        icon = {
+                            Image(
+                                painter = painterResource(id = R.drawable.today),
+                                contentDescription = null
+                            )
+                        },
+                        selected = navController.currentDestination?.route == "secondPage" || navController.previousBackStackEntry == null,
+                        onClick = { navController.navigate("secondPage") }
+                    ),
+                    BottomNavigationItem(
+                        icon = {
+                            Image(
+                                painter = painterResource(id = R.drawable.paintdiary),
+                                contentDescription = null
+                            )
+                        },
+                        selected = navController.currentDestination?.route == "thirdPage",
+                        onClick = { navController.navigate("thirdPage") }
+                    )
                 )
-            )
-            BottomNavigation(
-                items = bottomNavItems
-            )
+            if (showAppBarAndNavBar) {
+                BottomNavigation(
+                    items = bottomNavItems
+                )
+            }
         }
     }
 }
