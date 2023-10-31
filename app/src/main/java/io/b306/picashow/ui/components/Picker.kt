b8f699f828f0fdbd506e906d1fe8fdaab136a67c
박태흠 <xomamoc@naver.com>
2023-10-31 10:34:12 +0900

    feat: 일정 추가 페이지 개발

--- app/src/main/java/io/b306/picashow/ui/components/Picker.kt
+++ app/src/main/java/io/b306/picashow/ui/components/Picker.kt
@@ -2,19 +2,34 @@ package io.b306.picashow.ui.components
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.rounded.Build
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import java.util.Calendar
 
 @Composable
@@ -44,6 +59,8 @@ fun ShowDatePicker() {
     }
 }
 
+
+
 @Composable
 fun ShowTimePicker() {
     val context = LocalContext.current
@@ -67,5 +84,76 @@ fun ShowTimePicker() {
     )
     if (selectedTime.isNotEmpty()) {
         Text("Selected Date: $selectedTime")
+    }
+}
+
+@Composable
+fun CustomTimePicker(
+    selectedHour: MutableState<Int>,
+    selectedMinute: MutableState<Int>
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Transparent)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(150.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                itemsIndexed(items = (0..23).toList()) { _, hour ->
+                    Text(
+                        text = hour.toString().padStart(2, '0'),
+                        color = if (hour == selectedHour.value) Color.White else Color.Gray,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                selectedHour.value = hour
+                            }
+                            .padding(8.dp)
+                    )
+                }
+            }
+            // 여기서 ":" 문자를 추가
+            Text(
+                text = ":",
+                fontSize = 32.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .align(Alignment.CenterVertically)
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(150.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                // 5분 간격으로 표시 (0, 5, 10, ... 55)
+                itemsIndexed(items = (0..59 step 5).toList()) { _, minute ->
+                    Text(
+                        text = minute.toString().padStart(2, '0'),
+                        color = if (minute == selectedMinute.value) Color.White else Color.Gray,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                selectedMinute.value = minute
+                            }
+                            .padding(8.dp)
+                    )
+                }
+            }
+        }
     }
 }
