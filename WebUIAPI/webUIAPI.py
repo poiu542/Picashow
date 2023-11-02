dfbe21da04ea4ea0d4ed0862d22e536b262dbe93
조영헌 <dudgjs304321@google.com>
2023-11-02 14:24:16 +0900

    FEAT : 유저가 선호하는 로라 적용 로직 구현

--- WebUIAPI/webUIAPI.py
+++ WebUIAPI/webUIAPI.py
@@ -15,16 +15,18 @@ from PIL import Image
 from pymongo import MongoClient
 
 
+# 이미지 생성 요청 포맷
 class RequestBody(BaseModel):
     input_text: str
+    user_theme: str
 
 
-url = "http://127.0.0.1:7860/"
+# WebUI url = http://127.0.0.1:7860/
 
 app = FastAPI()
 
 # create API client with custom host, port
-api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
+WEB_UI_API = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
 
 
 # create API client with custom host, port and https
@@ -69,9 +71,29 @@ def s3_upload(image_bytes):
 # return f"https://{{자신이 설정한 버킷 이름}}.s3.{location}.amazonaws.com/{filename}"
 
 
+def get_value_from_key(theme_list, key):
+    for dictionary in theme_list:
+        if key in dictionary:
+            return dictionary[key]
+    return ""
+
+
+# 배경화면 생성 API
 @app.post("/image")
 def sendAPI(requestBody: RequestBody):
-    input = "'" + requestBody.input_text + "'을(를) 바탕으로 이미지 프롬프트를 영어로 만들어줘."
+    theme_list = [
+        {"PrgXL_V1": "<lora:PrgXL_V1:1>"},
+        {"Featastic2": "<lora:FaeTastic2:1> Faetastic"},
+        {"ghibli_last": "<lora:ghibli_last:1> ghibli"},
+        {"sdxl-1.0_makoto-shinkai": "<lora:sdxl-1.0_makoto-shinkai:1> by Makoto Shinkai"},
+        {"v0ng44g, p14nt1ng": "<lora:v0ng44g, p14nt1ng:1> v0ng44g, p14nt1ng"},
+        {"p1c4ss0_003-step00028000": "<lora:p1c4ss0_003-step00028000:1> p1c4ss0"},
+        {"edgRenaissanceXL": "<lora:edgRenaissanceXL:1> painted in renaissance style"}
+    ]
+
+    input = "'" + requestBody.input_text + "' 바탕으로 이미지 프롬프트를 영어로 만들어줘."
+
+    theme = get_value_from_key(theme_list, requestBody.user_theme)
 
     logger.info("test")
 
@@ -82,20 +104,20 @@ def sendAPI(requestBody: RequestBody):
             {"role": "user", "content": input}
         ],
     )
-    logger.info(response.choices[0].message['content'])
-    ai_image = api.txt2img(prompt=response.choices[0].message['content'],
-                           negative_prompt="(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
-                           seed=-1,
-                           cfg_scale=7,
-                           override_settings={
-                               # "sd_model_checkpoint": "majicmixRealistic_v7.safetensors [7c819b6d13]"
-                               "sd_model_checkpoint": "sd_xl_base_1.0.safetensors [31e35c80fc]"
-                           },
-                           sampler_index="DDIM",
-                           # sampler_index="DPM++ SDM",
-                           width=512,
-                           height=1024
-                           )
+    logger.info(theme + response.choices[0].message['content'])
+    ai_image = WEB_UI_API.txt2img(prompt=theme + response.choices[0].message['content'],
+                                  negative_prompt="(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3), NSFW",
+                                  seed=-1,
+                                  cfg_scale=7,
+                                  override_settings={
+                                      # "sd_model_checkpoint": "majicmixRealistic_v7.safetensors [7c819b6d13]"
+                                      "sd_model_checkpoint": "sd_xl_base_1.0.safetensors [31e35c80fc]"
+                                  },
+                                  sampler_index="DDIM",
+                                  # sampler_index="DPM++ SDM",
+                                  width=512,
+                                  height=1024
+                                  )
     image = ai_image.images[0]
     # with Image.open("./00014-99100504.png") as image:
     image_bytes = BytesIO()
@@ -107,10 +129,19 @@ def sendAPI(requestBody: RequestBody):
     return image_url
 
 
+# 배경화면 다운로드 시 유저 등록 API
+@app.post("/download")
+def registUser():
+    return True
+
+
+# 배경화면 조회 API
 @app.get("/list")
 def getList():
-    mongodb_URI = "mongodb://localhost:27017/"
+    mongodb_URI = "mongodb+srv://ldg03198:GZpxCobLh5To6L2k@wallpaper.vvwenhz.mongodb.net/?retryWrites=true&w=majority"
+    # mongodb_URI = "localhost:27017"
     client = MongoClient(mongodb_URI)
+    # collection = client.final.wallpaper
 
     collection = client['final']['wallpaper']
 
