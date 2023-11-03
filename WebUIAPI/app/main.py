ae968df09d78df2e82f48cc0a095e3583d329c3a
김영인 <bluest12@naver.com>
2023-11-03 16:16:21 +0900

    FIX : uvicorn 주석처리

--- WebUIAPI/app/main.py
+++ WebUIAPI/app/main.py
@@ -16,4 +16,4 @@ app = FastAPI()
 if __name__ == "__main__":
     app.include_router(mongodb_endpoint.router)
     app.include_router(webuiapi_endpoint.router)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    #uvicorn.run(app, host="0.0.0.0", port=8000)
