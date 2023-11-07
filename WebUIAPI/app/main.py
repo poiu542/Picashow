57548d72f615b391a2be9e0baed91e59dfd3155b
이대건 <ldg031@naver.com>
2023-11-07 12:52:08 +0900

    FEAT : cors 미들웨어 추가

--- WebUIAPI/app/main.py
+++ WebUIAPI/app/main.py
@@ -11,6 +11,7 @@ import uvicorn
 from fastapi import FastAPI
 import routers.webuiapi_endpoint as webuiapi_endpoint
 import routers.mongodb_endpoint as mongodb_endpoint
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 aws_access_key = os.getenv("LOCAL_AWS_S3_ACCESS_KEY")
@@ -19,6 +20,16 @@ aws_bucket_name = os.getenv("LOCAL_AWS_S3_BUCKET")
 aws_region = os.getenv("LOCAL_AWS_S3_REGION")
 
 app = FastAPI()
+
+# CORS 미들웨어 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 특정 도메인만 허용하려면 도메인 리스트를 명시합니다.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     app.include_router(mongodb_endpoint.router)
