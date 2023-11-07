11a4fdbf35423b126f184c1f6a104bd775607820
이대건 <ldg031@naver.com>
2023-11-07 12:42:13 +0900

    FIX : GPT 4 turbo로 교체 및 함수 수정

--- WebUIAPI/app/routers/webuiapi_endpoint.py
+++ WebUIAPI/app/routers/webuiapi_endpoint.py
@@ -8,6 +8,7 @@ import app.models.image_prompt as ImagePrompt
 import app.main as main
 import os
 import openai
+from openai import OpenAI
 from fastapi import APIRouter
 from urllib import request
 
@@ -41,21 +42,34 @@ def sendAPI(requestBody: ImagePrompt.ImagePrompt):
         {"edgRenaissanceXL": "<lora:edgRenaissanceXL:1> painted in renaissance style"}
     ]
 
-    input = "'" + requestBody.input_text + "' 바탕으로 이미지 프롬프트를 영어로 만들어줘."
+    input = "'" + requestBody.input_text + "' 바탕으로 이미지 프롬프트를 영어로 만들어줘. 다른 말은 하지마"
 
     theme = get_value_from_key(theme_list, requestBody.user_theme)
 
     # main.logger.info("test")
+    # api_key = os.getenv("OPENAI_API_KEY")
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",  # 사용할 모델 (현재 최신 모델)
+    response = openai.chat.completions.create(
+        model="gpt-4-1106-preview",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": input}
         ],
+
     )
-    # main.logger.info(theme + response.choices[0].message['content'])
-    ai_image = WEB_UI_API.txt2img(prompt=theme + response.choices[0].message['content'],
+
+    # response = openai.ChatCompletion.create(
+    #     model="gpt-4-1106-preview",  # 사용할 모델 (현재 최신 모델)
+    #     messages=[
+    #         {"role": "system", "content": "You are a helpful assistant."},
+    #         {"role": "user", "content": input}
+    #     ],
+    # )
+
+
+
+    print(response.choices[0].message.content)
+    ai_image = WEB_UI_API.txt2img(prompt=theme + response.choices[0].message.content,
                                   negative_prompt="(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3), NSFW",
                                   seed=-1,
                                   cfg_scale=7,
