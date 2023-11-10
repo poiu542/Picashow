7034cd8b51d23fc521d328233eac2393430467ad
김두현 <engus5860@naver,com>
2023-11-10 12:44:13 +0900

    FIX:  api 주소 수정

--- app/src/main/java/io/b101/picashow/api/ApiObject.kt
+++ app/src/main/java/io/b101/picashow/api/ApiObject.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object ApiObject {
     // 10.25. 미정된 상태
-    private const val BASE_URL = "http://k9b101.p.ssafy.io:8000/"
+    private const val BASE_URL = "http://k9b101.p.ssafy.io:8082/"
 
     private val loggingInterceptor by lazy {
         HttpLoggingInterceptor().apply {
